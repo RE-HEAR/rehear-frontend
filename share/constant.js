36eabb7dd6ffa65881f81ca6e7513fdc747a99d3
minjoolab <77260743+minjoolab@users.noreply.github.com>
2022-02-21 00:08:43 +0900

    Edit Login & footer
    feat : login & footer 
    login, footer 수정
    폴더 구조 변경

--- share/constant.js
+++ share/constant.js
@@ -10,22 +10,16 @@ function Constant(){
     const LockIcon = <LockIcon/>
 }
 
-// export const Colors = {
-//     MAIN_COLOR:'#A2A9CD',
-//     SECOND_COLOR: 'BFC9E7',
-//     FOOT_COLOR: 'FAF7F7',
+// export default function Foot(){
+//     return(
+//         <div class="footer" id="log-footer">
+//             <div style={{display:'block', width:'70%'}}>
+//                 <div id="foot">인터뷰 지원 | 제휴 문의 team.seoridam@gmail.com</div>
+//                 <div style={{color:'white'}}>(주) 서리담 | 대표 : 김태완 | 사업자 등록번호 : 123-45-67890 | 서울시 중구 필동로 1길 30 | 통신판매업신고 : 2021-서울중구-12345 | 이용약관 | 개인정보 처리방침 © REHEAR Inc.</div>
+//             </div>
+//         </div>
+//     );
 // }
-
-export default function Foot(){
-    return(
-        <div class="footer" id="log-footer">
-            <div style={{display:'block', width:'70%'}}>
-                <div id="foot">인터뷰 지원 | 제휴 문의 team.seoridam@gmail.com</div>
-                <div style={{color:'white'}}>(주) 서리담 | 대표 : 김태완 | 사업자 등록번호 : 123-45-67890 | 서울시 중구 필동로 1길 30 | 통신판매업신고 : 2021-서울중구-12345 | 이용약관 | 개인정보 처리방침 © REHEAR Inc.</div>
-            </div>
-        </div>
-    );
-}
 
 export {PersonOutlineIcon, LockIcon};
 
